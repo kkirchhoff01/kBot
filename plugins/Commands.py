0d3dbd2bab1ea3b609533f537604a0f0a2188070
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2016-01-12 18:59:03 -0600

    cleaned code

--- plugins/Commands.py
+++ plugins/Commands.py
@@ -6,9 +6,11 @@ import mechanize
 
 command_trigger = '.'
 
+
 def get_command(cmd, msg):
     if cmd == command_trigger + 'help':
-        return "For a list of commands use .commands. To use kBot try: .<command> <input>"
+        return("For a list of commands use .commands. To use kBot try:" +
+               " .<command> <input>")
     elif cmd == command_trigger + 'commands':
         return 'Commands: ' + str(get_command_list()).strip('[]')
     elif cmd == command_trigger + 'g':
@@ -25,9 +27,11 @@ def get_command(cmd, msg):
             try:
                 return conver_units(msg[0], msg[1], msg[2])
             except:
-                return "Usage: %sconvert <value> <units to convert from> <units to convert to>" % command_trigger
+                return("Usage: %sconvert <value> <units to convert from>" +
+                       " <units to convert to>" % command_trigger)
         else:
-            return "Usage: %sconvert <value> <units to convert from> <units to convert to>" % command_trigger
+            return("Usage: %sconvert <value> <units to convert from>" +
+                   " <units to convert to>" % command_trigger)
     elif cmd == command_trigger + 'eval':
         if ' ' in msg:
             lang = msg[0:msg.index(' ')]
@@ -39,50 +43,58 @@ def get_command(cmd, msg):
     else:
         return False
 
+
 def get_command_list():
     return [command_trigger + 'help', command_trigger + 'commands',
-            command_trigger + 'g', command_trigger + 'w', 
-            command_trigger + 'about', command_trigger + 'convert', 
+            command_trigger + 'g', command_trigger + 'w',
+            command_trigger + 'about', command_trigger + 'convert',
             command_trigger + 'eval', command_trigger + 'def']
+
 
 def google(search_input):
     if search_input == '':
-        return command_trigger +  'Use help for help.'
+        return command_trigger + 'Use help for help.'
     query = urllib.urlencode({'q': search_input})
     url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&' + query
     response = urllib.urlopen(url)
     search_results = response.read()
     input_results = json.loads(search_results)
     results = input_results['responseData']['results']
-    result = re.sub('<.*?>','', u' '.join((results[0]['title'], ': ', results[0]['url'], results[0]['content'])).encode('utf-8').strip())
+    result = re.sub('<.*?>', '', u' '.join((results[0]['title'], ': ',
+                    results[0]['url'],
+                    results[0]['content'])).encode('utf-8').strip())
     return result
 
-#Use wikipedia python library? Or do it the right way?
+
+# Use wikipedia python library? Or do it the right way?
 def wikipedia(search_input):
     if search_input == '':
         return 'Use '+command_trigger+'help for help.'
     return 'Wiki search is still in progress!'
 
-#Keep dict file or use website
-#Possibly find words most closely spelled to request
+
+# Keep dict file or use website
+# Possibly find words most closely spelled to request
 def dictionary_search(search_input):
-    if search_input.isalpha() == False or len(search_input.split(' ')) > 1 or search_input == '':
+    if not search_input.isalpha() or len(search_input.split(' ')) > 1 or search_input == '':
         return 'Invalid input.'
     else:
         url = 'http://dictionary.reference.com/browse/' + search_input
         response = urllib.urlopen(url)
         search_results = response.read()
-        content = re.findall(r'<div class="def-content">(.*?)</div>', search_results, re.DOTALL)
+        content = re.findall(r'<div class="def-content">(.*?)</div>',
+                             search_results, re.DOTALL)
         if len(content) != 0:
-            return_content = re.sub('<.*?>','', content[0])
+            return_content = re.sub('<.*?>', '', content[0])
             return return_content.strip('\n')
         else:
             return 'Definition not found'
 
-#Get code from submit site using form info
+
+# Get code from submit site using form info
 def evaluate(language, eval_input):
     if eval_input == '':
-        return 'Use '+command_trigger+'help for help.'
+        return 'Use ' + command_trigger + 'help for help.'
     value = ""
     if language.lower() == 'c':
         value = "c/gcc-4.4.3"
@@ -97,11 +109,11 @@ def evaluate(language, eval_input):
     elif language.lower() == 'perl':
         value = "perl/perl-5.20.0"
     elif language.lower() == 'python' or language.lower() == 'python2':
-        value="python/cpython-2.7.8"
+        value = "python/cpython-2.7.8"
     elif language.lower() == 'python3':
-        value="python/cpython-3.4.1"
+        value = "python/cpython-3.4.1"
     elif language.lower() == 'ruby':
-        value="ruby/mri-1.0"
+        value = "ruby/mri-1.0"
     elif language.lower() == 'list':
         c = 'Language Options: C, C++, Javascript, lua, PHP, Perl, Python, Python3, Ruby.'
         return c
@@ -118,10 +130,11 @@ def evaluate(language, eval_input):
     content = content.split('Output')[2].split('Fork')[0]
     br.close()
     content = re.findall(r'<p.*?>(.*?)</p.*?>', content, re.DOTALL)
-    if 'OK' in content[len(content) -1]:
+    if 'OK' in content[len(content) - 1]:
         return content[0].strip('\n')
     else:
         return content[len(content)-1].strip('\n')
+
 
 def convert_units(value, unit_input, unit_output):
     if value == '':
--- plugins/link_reader.py
+++ plugins/link_reader.py
@@ -4,12 +4,14 @@ import json
 import re
 from bs4 import BeautifulSoup
 
+
 def check_link(msg):
     links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', msg)
     if len(links) > 0:
         return links[0]
     else:
         return False
+
 
 def read_link(url):
     try:
