ff1aadf29a6b129ae29107587cbac5c4c3ed0569
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2016-01-18 17:06:22 -0600

    fixed link reader

--- plugins/link_reader.py
+++ plugins/link_reader.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 
 def check_link(msg):
-    links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', msg)
+    links = re.findall('(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', msg)
     if len(links) > 0:
         return links[0]
     else:
@@ -15,18 +15,20 @@ def check_link(msg):
 
 def read_link(url):
     try:
+        if re.match('http[s]?://', url) == None:
+            url = 'http://' + url
         response = urllib.urlopen(url)
         results = response.read()
+        response.close()
         info = response.info()
         info_type = info.type.split('/')
         if info_type[1] == 'html':
-            results = response.read()
             soup = BeautifulSoup(results)
             url_title = soup.title.string
             return url_title.encode('utf-8')
         else:
             return '[' + info_type[0] + '][' + info_type[1] + '] ' + info.getheaders('Content-Length')[0] + ' bytes'
-    except IOError:
+    except IOError, exc:
         print exc
         return False
     except Exception, exc:
