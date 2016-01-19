6f3d788700426ac406f094b6b41e1584099cb136
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2016-01-18 21:00:40 -0600

    improved linkreader

--- plugins/link_reader.py
+++ plugins/link_reader.py
@@ -4,7 +4,6 @@ import json
 import re
 from bs4 import BeautifulSoup
 
-
 def check_link(msg):
     links = re.findall('(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', msg)
     if len(links) > 0:
@@ -21,13 +20,21 @@ def read_link(url):
         results = response.read()
         response.close()
         info = response.info()
-        info_type = info.type.split('/')
-        if info_type[1] == 'html':
+        if info.type == 'text/html':
             soup = BeautifulSoup(results)
             url_title = soup.title.string
             return url_title.encode('utf-8')
         else:
-            return '[' + info_type[0] + '][' + info_type[1] + '] ' + info.getheaders('Content-Length')[0] + ' bytes'
+            info_type = info.type.split('/')
+            content_size = float(info.getheaders('Content-Length')[0])
+            if content_size < 1000.0:
+                return '[' + info_type[0] + '][' + info_type[1] + '] ' + str(content_size) + ' B'
+            elif content_size > 1000.0 and content_size < 1000000.0:
+                return '[' + info_type[0] + '][' + info_type[1] + '] ' + str(content_size/1000.0) + ' Kb'
+            elif content_size > 1000000.0 and content_size < 1000000000.0:
+                return '[' + info_type[0] + '][' + info_type[1] + '] ' + str(content_size/1000000.0) + ' Mb'
+            elif content_size > 1000000000.0:
+                return '[' + info_type[0] + '][' + info_type[1] + '] ' + str(content_size/1000000000.0) + ' Gb'
     except IOError, exc:
         print exc
         return False
