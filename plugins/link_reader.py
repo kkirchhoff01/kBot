298ce4a594cb8ee444196f95349356b868dfb770
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2016-01-18 21:43:36 -0600

    cleaned link reader

--- plugins/link_reader.py
+++ plugins/link_reader.py
@@ -25,7 +25,6 @@ def read_link(url):
             url_title = "[URL] %s" % soup.title.string
             return url_title.encode('utf-8')
         else:
-<<<<<<< HEAD
             content_size = float(info.getheaders('Content-Length')[0])
             if content_size < 1000.0:
                 return "[%s] %.2f B" % (info.type, content_size)
@@ -35,18 +34,6 @@ def read_link(url):
                 return "[%s] %.2f MB" % (info.type, content_size/1000000.0)
             elif content_size > 1000000000.0:
                 return "[%s] %.2f GB" % (info.type, content_size/1000000000.0)
-=======
-            info_type = info.type.split('/')
-            content_size = float(info.getheaders('Content-Length')[0])
-            if content_size < 1000.0:
-                return '[' + info_type[0] + '][' + info_type[1] + '] ' + str(content_size) + ' B'
-            elif content_size > 1000.0 and content_size < 1000000.0:
-                return '[' + info_type[0] + '][' + info_type[1] + '] ' + str(content_size/1000.0) + ' Kb'
-            elif content_size > 1000000.0 and content_size < 1000000000.0:
-                return '[' + info_type[0] + '][' + info_type[1] + '] ' + str(content_size/1000000.0) + ' Mb'
-            elif content_size > 1000000000.0:
-                return '[' + info_type[0] + '][' + info_type[1] + '] ' + str(content_size/1000000000.0) + ' Gb'
->>>>>>> 6f3d788700426ac406f094b6b41e1584099cb136
     except IOError, exc:
         print exc
         return False
