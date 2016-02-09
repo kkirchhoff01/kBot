f86833817a227a2d323998f9048133d3dd8b5951
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2016-02-09 13:22:36 -0600

    better formatting

--- plugins/translator.py
+++ plugins/translator.py
@@ -8,8 +8,9 @@ with open('key.txt', 'r') as fh:
 
 url = language_detection = "https://translate.yandex.net/api/v1.5/tr.json/"
 
+
 def translate(text):
-    language_detection = url + "detect?key={}&text={}".format(api_key, text)
+    language_detection = url + "detect?key={0}&text={1}".format(api_key, text)
     response = urllib.urlopen(language_detection)
     detection_results = response.read()
     detection_json = json.loads(detection_results)
@@ -19,7 +20,7 @@ def translate(text):
         return "Failed to detect language"
 
     lang = detection_json['lang'] + "-en"
-    translator = url + "translate?key={}&text={}&lang={}".format(
+    translator = url + "translate?key={0}&text={1}&lang={2}".format(
             api_key, text, lang)
     response = urllib.urlopen(translator)
     results = response.read()
@@ -29,4 +30,4 @@ def translate(text):
     if int(translation['code']) != 200:
         return "Failed to translate"
 
-    return "({}) {}".format(lang, translation['text'][0])
+    return "({0}) {1}".format(lang, translation['text'][0])
