7f09b07e2826ee55ea8a1ff68aefc7b799127067
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2016-02-13 13:22:19 -0600

    added comments and minor fixes

--- plugins/image_ascii.py
+++ plugins/image_ascii.py
@@ -3,62 +3,78 @@ import cStringIO
 import urllib
 from PIL import Image
 
-#####################################################################
-#                                                                   #
-# This code was inspired by Jalal Sela's ascii draw found at:       #
-# https://jalalsela.wordpress.com/2015/03/19/modigliani-an-irc-bot/ #
-#                                                                   #
-#####################################################################
+# IRC color options and their RGB values
+colors = {0: (255, 255, 255),
+          1: (0, 0, 0),
+          2: (0, 0, 127),
+          3: (0, 147, 0),
+          4: (255, 0, 0),
+          5: (127, 0, 0),
+          6: (156, 0, 156),
+          7: (252, 127, 0),
+          8: (255, 255, 0),
+          9: (0, 252, 0),
+          10: (0, 147, 147),
+          11: (0, 255, 255),
+          12: (0, 0, 252),
+          13: (255, 0, 255),
+          14: (127, 127, 127),
+          15: (210, 210, 210)
+          }
 
-colors = { 0 : (255, 255, 255),
-           1 : (0, 0, 0),
-           2 : (0, 0, 127),
-           3 : (0, 147, 0),
-           4 : (255, 0, 0),
-           5 : (127, 0, 0),
-           6 : (156, 0, 156),
-           7 : (252, 127, 0),
-           8 : (255, 255, 0),
-           9 : (0, 252, 0),
-           10 : (0, 147, 147),
-           11 : (0, 255, 255),
-           12 : (0, 0, 252),
-           13 : (255, 0, 255),
-           14 : (127, 127, 127),
-           15 : (210, 210, 210)
-        }
 
+# Draw Image
 def draw_ascii(im_link):
-    zonebounds = range(4,256,4)
+    zonebounds = range(4, 256, 4)
     url_string = ''
 
     try:
+        # Get Image data
         imgfile = cStringIO.StringIO(urllib.urlopen(im_link).read())
         im = Image.open(imgfile)
+
+        # Resize image
         height = (im.size[1] * 20) / (im.size[0])
         im = im.resize((50, height), Image.ANTIALIAS)
+
+        # Get image in RGB values
         im_col = im.convert('RGB')
+
         imgBuf = ""
-        for y in range(0,im.size[1]):
+        for y in range(0, im.size[1]):
             imgBuf = ""
-            for x in range(0,im.size[0]):
-                light = get_color(im_col.getpixel((x,y)))
-                imgBuf  = imgBuf + ("\x03" + str(light) + "," +
-                                    str(light)  + "x\x03")
+            for x in range(0, im.size[0]):
+                # Get closest color
+                light = get_color(im_col.getpixel((x, y)))
+                imgBuf = imgBuf + ("\x03" + str(light) + "," +
+                                   str(light) + "x\x03")  # Format for IRC
+
+            # Add colored character to final string
             url_string = url_string + str(imgBuf) + '\n'
+
         return url_string
+
     except:
         return "Image failed to draw"
 
 
+# Find IRC color option with closest RGB value
 def get_color(pixel_color):
+    # Find the distance between the first color
     color_diff = [colors[0][i] - pixel_color[i] for i in xrange(3)]
     smallest_diff = np.linalg.norm(color_diff)
+
     closest_color = 0
     for c in colors.keys():
+        # Take the difference between each of the RGB values
         color_diff = [colors[c][i] - pixel_color[i] for i in xrange(3)]
+
+        # Use the norm of the RGB differences to get distance of color values
         diff = np.linalg.norm(color_diff)
+
+        # Find difference with smallest norm
         if diff < smallest_diff:
             closest_color = c
             smallest_diff = diff
+
     return closest_color
--- plugins/link_reader.py
+++ plugins/link_reader.py
@@ -4,29 +4,50 @@ import json
 import re
 from bs4 import BeautifulSoup
 
+
+# Check for URL in IRC message
 def check_link(msg):
-    links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', msg)
+    # Regex for URL
+    links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|' +
+                       '[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', msg)
+
+    # If URL was found len(lings) > 0
     if len(links) > 0:
-        return links[0]
+        return links[0]  # Return first URL (don't want multiple searches)
     else:
-        return False
+        return False  # No URL found
 
 
+# Function to fetch link info
 def read_link(url):
     try:
+        # Open link and get response
         response = urllib.urlopen(url)
         results = response.read()
         response.close()
+
         info = response.info()
+
+        # URL is text/html web page
         if info.type == 'text/html':
+            # Use BeautifulSoup to get info
             soup = BeautifulSoup(results)
             title_string = re.sub('[\r|\n]', ' ', soup.title.string)
+
+            # Handle page summary longer than 200 characters
             if len(title_string) > 200:
                 title_string = title_string[:200] + "..."
-            url_title = "[URL] {}".format(title_string)
+
+            # Return page info
+            url_title = "[URL] {0}".format(title_string)
             return url_title.encode('utf-8')
+
+        # URL is not HTML (download or image)
         else:
+            # Get size of content
             content_size = float(info.getheaders('Content-Length')[0])
+
+            # Format byte size to KB/MB/etc.
             if content_size < 1000.0:
                 return "[%s] %.2f B" % (info.type, content_size)
             elif content_size > 1000.0 and content_size < 1000000:
@@ -35,10 +56,13 @@ def read_link(url):
                 return "[%s] %.2f MB" % (info.type, content_size/1000000.0)
             elif content_size > 1000000000.0:
                 return "[%s] %.2f GB" % (info.type, content_size/1000000000.0)
-    except IOError, exc:
+
+    except IOError, exc:  # Bad link
         return "[URL] Site not found"
-    except AttributeError:
+
+    except AttributeError:  # No title information
         return "[URL] (No title)"
-    except Exception, exc:
+
+    except Exception, exc:  # Other exception; print error; return False
         print exc
         return False
